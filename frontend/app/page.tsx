4e767df088ecaa24c7376954f1c8320cd86545b2
Alex Shaw <alexandergshaw@gmail.com>
2025-08-25 21:51:36 +0000

    changed name on portfolio

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -473,7 +473,7 @@ export default function Home() {
                     lineHeight: 1.2,
                   }}
                 >
-                  Hi, I&apos;m John Doe
+                  Hi, I&apos;m Alex Shaw
                   {/* Job title with gradient effect */}
                   <Typography
                     variant="h2"
